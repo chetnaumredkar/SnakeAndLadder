39acd044932a0b26455d556eb68c8c1f2626d4a0
kolhechetna <kolhe.chetna@gmail.com>
2022-10-31 18:32:18 +0530

    Added:The Player then checks for a Option.

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -7,5 +7,23 @@ public class SnakeLadder {
 		System.out.println("-------------------------------------");
 		 int dice_roll=(int)(Math.random()*6+1);
 			System.out.println("Dice Rolled :"+dice_roll);
+			int Step_Count=0;
+			int options=(int)(Math.random()*3+1);
+			System.out.println("option is"+options);
+			if(options==1) {
+				System.out.println("No Play");
+				Step_Count=Step_Count;
+			}
+			else if(options==2) {
+				System.out.println("there is ladder");
+				Step_Count+=dice_roll;
+			}
+			else if(options==3) {
+				System.out.println("there is Snake");
+				Step_Count-=dice_roll;
+			}
+			else {
+					System.out.println("roll dice again");
+				}
 	}
 }
