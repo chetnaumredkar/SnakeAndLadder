1db13ccb4f50c9265459fd46298636fa78377f6e
kolhechetna <kolhe.chetna@gmail.com>
2022-10-31 18:22:04 +0530

    Added:Starting of Program

---
+++ src/com/bridgelabz/SnakeLadder.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class SnakeLadder {
+	public static void main(String[] args) {
+		System.out.println("starting of Snake and Ladder Game");
+		System.out.println("Single Player at position 0");
+	}
+}
