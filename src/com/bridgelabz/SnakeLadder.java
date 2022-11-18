6fe77a84d4b44152d782b5d8f516f78d5571779f
chetnaumredkar <111214723+chetnaumredkar@users.noreply.github.com>
2022-11-18 15:53:41 +0530

    Update SnakeLadder.java

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -4,8 +4,8 @@ public class SnakeLadder {
 	
 		
 		public static void main(String[] args) {
-			System.out.println("starting of Snake and Ladder Game");
-			System.out.println("Single Player at position 0");
+		System.out.println("starting of Snake and Ladder Game");
+		System.out.println("Single Player at position 0");
 		int step_Count_Player1=0;
 		int step_Count_Player2=0;
 		int count_Of_Dice_One=0;
